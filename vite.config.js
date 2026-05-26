c6907a3e93afc037902542a6927d49f6a4eb1861
Davison Campos <davison.campos1@gmail.com>
2026-05-26 15:03:32 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://davisoncdev.github.io/lets-organize/'
+  base: '/lets-organize/'
 })
